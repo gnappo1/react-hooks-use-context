e4c0f00242d89db94a7f9e3a26c48e2307aa8b04
Matteo <gnappo.1@live.it>
2021-12-14 14:01:42 -0800

    Remove prop drilling with composition

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Profile from "./Profile";
+import ThemedButton from "./ThemedButton";
+import DarkModeToggle from "./DarkModeToggle";
+import Interests from "./Interests";
+import defaultUser from "../data";
 
 function App() {
   const [theme, setTheme] = useState("dark");
   const [user, setUser] = useState(null);
+
+  function handleLogin() {
+    if (user) {
+      setUser(null);
+    } else {
+      setUser(defaultUser);
+    }
+  }
   return (
     <main className={theme}>
-      <Header theme={theme} setTheme={setTheme} user={user} setUser={setUser} />
-      <Profile theme={theme} user={user} />
+      <Header user={user}>
+        <ThemedButton onClick={handleLogin}>
+          {user ? "Logout" : "Login"}
+        </ThemedButton>
+        <DarkModeToggle theme={theme} setTheme={setTheme} />
+      </Header>
+      <Profile user={user}>
+        <Interests interests={user ? user.interests : ["You must Log In First"]} theme={theme} />
+      </Profile>
     </main>
   );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Interests from "./Interests";
 
-function Profile({ user, theme }) {
+function Profile({ user, children }) {
   if (!user) return <h2>Please Login To View Profile</h2>;
   return (
     <div>
-      <h2>{user.name}'s Profile</h2>
-      <Interests interests={user.interests} theme={theme} />
+      {user && <h2>{user.name}'s Profile</h2>}
+      {children}
     </div>
   );
 }
